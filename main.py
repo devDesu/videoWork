bd02ad8c39d8f771c61bc9687819641e0f8bd5c1
devDesu <cuzmenko.tosha@gmail.com>
2015-06-01 23:56:10 +0300

    Now able to build videos from images. Going to add GUI tomorrow and fix some bugs

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 # -*- encode: utf-8 -*-
+import time
+
 __author__ = 'Anton'
+import cv2
 import struct
 import os
-import exceptions
-import io
+import numpy as np
 
 
 class Gluer:
@@ -13,6 +16,7 @@ class Gluer:
         self.out_name = out_name
         self.minutes = []
         self.total = None
+        self.size = []
 
     def check_size(self):
         size = None
@@ -26,7 +30,10 @@ class Gluer:
                     else:
                         temp_size = self.sze(path, path[path.index('.', len(path)-4)+1:])
                     if temp_size[0] != size[0] or temp_size[1] != size[1]:
-                        raise Exception('Image size error', path, size, temp_size)
+                        #raise Exception('Image size error', path, size, temp_size)
+                        if temp_size[0] > size[0]: size[0] = temp_size[0]
+                        if temp_size[1] > size[1]: size[1] = temp_size[1]
+        self.size = size
 
     def get_minutes(self):
         self.minutes = os.listdir(self.path)  # 0 to 12, minutes
@@ -42,14 +49,16 @@ class Gluer:
             self.minutes[key] = {k2: os.listdir(os.path.join(self.path, key, k2)) for k2 in self.minutes[key]}
             for key2, value2 in self.minutes[key].iteritems():
                 self.minutes[key][key2].sort(key=lambda x: len(x))
-                if len(value2) < self.fps and not (counter2 == 0 or counter2 == 59):
+                if len(value2) < self.fps and not (counter == len(key2)):
                     raise Exception('fps error', len(value2), os.path.join(self.path, key, key2))
                 counter2 += 1
             total += counter2
+            #if total >= 48:
+            #    self.total = 48
+            #    return
         self.total = total
 
     def sze(self, filename, ftype):
-        print ftype
         with open(os.path.join(self.path, filename), 'rb') as i:
             if ftype == 'bmp':
                 i.seek(18)
@@ -63,8 +72,22 @@ class Gluer:
                 if check != 0x0d0a1a0a:
                     return
                 width, height = struct.unpack('>ii', head[16:24])
+            else:
+                width, height = 720, 1280
             return [width, height]
 
-h = Gluer(u'C:/Users/Public/test2')
+h = Gluer(u'G:\\fight')
 h.get_minutes()
 h.check_size()
+fourcc = -1
+out = cv2.VideoWriter(u'G:\\out.avi'.encode('utf-8'), fourcc, 12.0, (h.size[1], h.size[0]),  True)
+for key, value in h.minutes.iteritems():
+    for key2, value2 in value.iteritems():
+        for filename in value2:
+            frame = cv2.imread(os.path.join(h.path, key, key2, filename).encode("utf-8"),
+                               flags=cv2.cv.CV_LOAD_IMAGE_COLOR)
+            #frame.resize(h.size)
+            time.sleep(1)
+            print frame.shape
+            out.write(frame)
+out.release()
